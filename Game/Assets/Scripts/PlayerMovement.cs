32354f62a7d7e432f3974655b086e23bd595c1b4
Liam Waters <liamwats@outlook.com>
2021-12-13 10:09:58 +0000

    Turn Based Mechanics

--- Game/Assets/Scripts/PlayerMovement.cs
+++ Game/Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviourPun, IPunObservable
 {
@@ -17,6 +19,7 @@ public class PlayerMovement : MonoBehaviourPun, IPunObservable
     public LayerMask whatIsGround;
 
     public bool isGrounded;
+    public int lobbyScene = 1;
 
     private Vector3 smoothMove;
 
@@ -30,11 +33,20 @@ public class PlayerMovement : MonoBehaviourPun, IPunObservable
     private Sprite[] sprites;
     public ArrayList takenSprites = new ArrayList();
 
+    public Queue<string> playerTurnOrder;
     public Text playerNameText;
+    [SerializeField]
+    public bool isTurn = false;
+    [SerializeField]
+    Button waitTurn;
+    [SerializeField]
+    Button endTurn;
 
     void Start()
     {
+        playerTurnOrder = new Queue<string>();
         rigidbody = GetComponent<Rigidbody2D>();
+        
         if (photonView.IsMine)
         {
             playerNameText.text = PhotonNetwork.NickName;
@@ -48,6 +60,25 @@ public class PlayerMovement : MonoBehaviourPun, IPunObservable
             playerNameText.text = pv.Owner.NickName;
         }
 
+        waitTurn = GameObject.Find("WaitTurnButton").GetComponent<Button>();
+        endTurn = GameObject.Find("EndTurnButton").GetComponent<Button>();
+
+        endTurn.onClick.AddListener(() => endPlayerTurn()); ; //.......................................................................................
+
+        Debug.Log(PhotonNetwork.IsMasterClient);
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("We reached here");
+            //Add all players to queue.
+            //foreach (Player player in PhotonNetwork.PlayerList)
+            //{
+            //    Debug.Log("loop");
+            //    playerTurnOrder.Enqueue(player.NickName);
+                
+            //}
+            //string whosTurn = playerTurnOrder.Dequeue();
+            pv.RPC("RPCswitchTurn", RpcTarget.Others);     //change to RPCendPlayerTurn
+        }
         
     }
 
@@ -62,6 +93,24 @@ public class PlayerMovement : MonoBehaviourPun, IPunObservable
         {
             smoothMovement();
         }
+
+        try
+        {
+            Vector3 pos = waitTurn.transform.position;
+            if (isTurn)
+            {
+                waitTurn.gameObject.SetActive(false);
+            }
+            else
+            {
+                waitTurn.gameObject.SetActive(true);
+            }
+        }
+        catch(System.NullReferenceException e)
+        {
+            //Object empty
+        }
+        
     }
 
     private void smoothMovement()
@@ -71,40 +120,50 @@ public class PlayerMovement : MonoBehaviourPun, IPunObservable
 
     private void ProcessInputs()
     {
+
         var move = new Vector3(Input.GetAxis("Horizontal"), 0);
-        transform.position += move * speed * Time.deltaTime;
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        
+        if (isTurn)
         {
-            spriteRenderer.flipX = true;
-            pv.RPC("OnDirectionChange_LEFT", RpcTarget.Others);
-        }
+            transform.position += move * speed * Time.deltaTime;
 
-        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
-        {
-            spriteRenderer.flipX = false;
-            pv.RPC("OnDirectionChange_RIGHT",RpcTarget.Others);
-        }
-
-        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
-
-        if (isGrounded && (Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)))
-        {
-            rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Mouse1))
-        {
-            if (spriteRenderer.flipX == true)
+            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
             {
-                PhotonNetwork.Instantiate(Shurican.name, Throwpoint.position, Throwpoint.rotation);//Throw shurican
+                spriteRenderer.flipX = true;
+                pv.RPC("OnDirectionChange_LEFT", RpcTarget.Others);
             }
-            else
+
+            if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
             {
-                PhotonNetwork.Instantiate(Shurican.name, Throwpoint.position, Throwpoint.rotation);//Throw shurican
+                spriteRenderer.flipX = false;
+                pv.RPC("OnDirectionChange_RIGHT", RpcTarget.Others);
+            }
+
+            isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+
+            if (isGrounded && (Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)))
+            {
+                rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                if (spriteRenderer.flipX == true)
+                {
+                    PhotonNetwork.Instantiate(Shurican.name, Throwpoint.position, Throwpoint.rotation);//Throw shurican
+                }
+                else
+                {
+                    PhotonNetwork.Instantiate(Shurican.name, Throwpoint.position, Throwpoint.rotation);//Throw shurican
+                }
             }
         }
-
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PhotonNetwork.LeaveRoom();
+            SceneManager.LoadScene(lobbyScene);
+        }
+            
         //Change colour of player if not all sprites are taken.
         if (Input.GetKeyDown(KeyCode.C))
         {
@@ -113,8 +172,7 @@ public class PlayerMovement : MonoBehaviourPun, IPunObservable
 
             pv.RPC("changePlayerColour", RpcTarget.Others, index);
 
-        }
-
+        }    
     }
 
     public int randomPlayerColour()
@@ -132,6 +190,35 @@ public class PlayerMovement : MonoBehaviourPun, IPunObservable
         takenSprites.Add(sprites[index]);
         spriteRenderer.sprite = sprites[index];
         return index;
+    }
+
+    public void endPlayerTurn()
+    {
+        //Debug.Log("endturn");
+        //playerTurnOrder.Enqueue(PhotonNetwork.LocalPlayer.NickName);
+        //string whosTurn = playerTurnOrder.Dequeue();
+        isTurn = false;
+        pv.RPC("RPCswitchTurn", RpcTarget.Others); //switch to RPCendPlayerTurn
+    }
+
+    //[PunRPC]
+    //public void RPCendPlayerTurn(Queue<string> playerTurnOrder, string whosTurn)
+    //{
+    //    if (PhotonNetwork.NickName == whosTurn)
+    //    {
+    //        isTurn = true;
+    //    }
+    //    else
+    //    {
+    //        isTurn = false;
+    //    }
+    //}
+
+    
+        [PunRPC]
+    public void RPCswitchTurn()
+    {
+         isTurn = true;
     }
 
     [PunRPC]
